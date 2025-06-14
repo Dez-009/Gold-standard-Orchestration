de5ce04468f0558eab9e8dc467e219dca62b7fb2
Dez_the_Dev <bshcontracts@proton.me>
2025-06-14 14:50:05 -0400

    Initialize database tables at startup

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 from config import settings
 from routes import router as api_router
 from routes.root import router as root_router
+from database.base import Base
+from database.session import engine
+
+# Create database tables on startup
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title=settings.project_name, version="0.1.0")
