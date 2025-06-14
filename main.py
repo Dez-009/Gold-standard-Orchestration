c4178ded5d7f4cb1279c86c32eaea2f7b5a8ce63
Dez_the_Dev <bshcontracts@proton.me>
2025-06-14 19:10:51 -0400

    Add journal routes and tests

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.user import router as user_router
 from routes.auth import router as auth_router
 from routes.protected import router as protected_router
 from routes.session import router as session_router
+from routes.journal import router as journal_router
 from routes.notification import router as notification_router
 from routes.vida import router as vida_router
 from routes.root import router as root_router
@@ -21,6 +22,7 @@ app.include_router(user_router)
 app.include_router(auth_router)
 app.include_router(protected_router)
 app.include_router(session_router)
+app.include_router(journal_router)
 app.include_router(notification_router)
 app.include_router(vida_router)
 app.include_router(root_router)
