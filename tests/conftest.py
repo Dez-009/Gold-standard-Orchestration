008848eaefb3c9ab209b1a09f2c0317800df4f50
Dez_the_Dev <bshcontracts@proton.me>
2025-06-14 17:27:17 -0400

    Add test database fixture

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from database.base import Base
+from main import app
+from database.utils import get_db
+from fastapi.testclient import TestClient
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_vida.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db: Session = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+        yield c
